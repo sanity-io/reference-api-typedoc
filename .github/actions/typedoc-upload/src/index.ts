1faecf7d49868e3a4471ce5261357f030bac44f9
Binoy Patel <me@binoy.io>
2025-04-29 22:34:11 -0400

    chore: push

--- .github/actions/typedoc-upload/src/index.ts
+++ .github/actions/typedoc-upload/src/index.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import fs from 'fs/promises'
+import path from 'path'
 
 async function run() {
   const packageName = core.getInput('packageName')
@@ -9,6 +10,7 @@ async function run() {
   console.log(`Uploading Typedoc JSON for ${packageName} v${version}`)
   console.log(typedocJsonPath)
 
+  console.log(__dirname, path.join(__dirname, typedocJsonPath))
   const typedocJson = await fs.readFile(typedocJsonPath, 'utf-8')
   console.log(typedocJson)
 }
