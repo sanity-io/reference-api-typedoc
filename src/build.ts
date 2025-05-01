3a03b5c6912bebc53c5ea71bfb65dcd99beaf574
Binoy Patel <me@binoy.io>
2025-05-01 11:32:39 -0400

    feat: add built to read typedoc json from remote

---
+++ src/build.ts
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+import fs from "fs/promises";
+dotenv.config();
+
+import { createClient } from "@sanity/client";
+
+const client = createClient({
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
+  apiVersion: "2021-03-25",
+});
+
+const query = `*[_type == 'typesReference'] {
+  ...,
+  "latestVersion": latestVersion->semver,
+  "typeDocJson": latestVersion->typedocJson.code
+}`;
+
+const data = await client.fetch(query);
+
+// Remove the input-docs folder if it exists at the root
+await fs.rm("input-docs", { recursive: true, force: true });
+
+// Create the input-docs folder
+await fs.mkdir("input-docs");
+
+for (const item of data) {
+  const dirname = item.slug.current.replace(/\//g, "-");
+
+  // Make a directory
+  await fs.mkdir(`input-docs/${dirname}`);
+
+  // Create a json file with the typeDocJson
+  await fs.writeFile(
+    `input-docs/${dirname}/typedoc.json`,
+    item.typeDocJson,
+    "utf-8"
+  );
+}
