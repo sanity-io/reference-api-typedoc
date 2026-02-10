93ff374d84edb8aabe4d4273326ffa0533e4510c
Mark Michon <mark.michon@sanity.io>
2026-02-09 19:03:25 -0800

    chore: catch errors in main run function

--- .github/actions/typedoc-upload/dist/index.js
+++ .github/actions/typedoc-upload/dist/index.js
@@ -57,59 +57,64 @@ const client = (0, client_1.createClient)({
     perspective: 'raw',
 });
 async function run() {
-    const packageName = core.getInput('packageName');
-    const version = core.getInput('version');
-    const typedocJsonPath = core.getInput('typedocJsonPath');
-    core.info(`[Uploading] Typedoc JSON for ${packageName} v${version}`);
-    // Upload the file as an asset
-    const fileStream = await promises_1.default.readFile(typedocJsonPath);
-    const uploadedAsset = await client.assets.upload('file', fileStream, {
-        filename: `${packageName}-v${version}-typedoc.json`,
-        contentType: 'application/json',
-    });
-    const query = (0, groq_1.default) `*[_type == "apiPlatform" && npmName == $name][0]`;
-    const platform = await client.fetch(query, { name: packageName });
-    if (!platform) {
-        core.setFailed(`Platform ${packageName} not found. Make sure you have a corresponding "API and platform" document in the Admin Studio: https://admin.sanity.io/structure/docs;changelog;apiPlatform`);
-        return;
-    }
-    const existingVersionsQuery = (0, groq_1.default) `*[_type == "apiVersion" && platform._ref == $platformId && semver == $semver]{ _id }`;
-    const existingVersions = await client.fetch(existingVersionsQuery, {
-        platformId: platform._id,
-        semver: version,
-    });
-    const attachment = {
-        _type: 'file',
-        asset: {
-            _type: 'reference',
-            _ref: uploadedAsset._id,
-        },
-    };
-    let res;
-    if (existingVersions.length > 0) {
-        core.info(`[Patching] ${existingVersions.length} existing apiVersion document(s)`);
-        const transaction = client.transaction();
-        for (const doc of existingVersions) {
-            core.info(`  - ${doc._id}`);
-            transaction.patch(doc._id, { set: { attachment } });
-        }
-        res = await transaction.commit();
-    }
-    else {
-        core.info(`[Creating] New apiVersion document for ${packageName} v${version}`);
-        res = await client.create({
-            _type: 'apiVersion',
-            date: new Date().toISOString().split('T')[0],
-            platform: {
-                _type: 'reference',
-                _ref: platform._id,
-            },
-            semver: version,
-            attachment,
+    try {
+        const packageName = core.getInput('packageName');
+        const version = core.getInput('version');
+        const typedocJsonPath = core.getInput('typedocJsonPath');
+        core.info(`[Uploading] Typedoc JSON for ${packageName} v${version}`);
+        // Upload the file as an asset
+        const fileStream = await promises_1.default.readFile(typedocJsonPath);
+        const uploadedAsset = await client.assets.upload('file', fileStream, {
+            filename: `${packageName}-v${version}-typedoc.json`,
+            contentType: 'application/json',
         });
+        const query = (0, groq_1.default) `*[_type == "apiPlatform" && npmName == $name][0]`;
+        const platform = await client.fetch(query, { name: packageName });
+        if (!platform) {
+            core.setFailed(`Platform ${packageName} not found. Make sure you have a corresponding "API and platform" document in the Admin Studio: https://admin.sanity.io/structure/docs;changelog;apiPlatform`);
+            return;
+        }
+        const existingVersionsQuery = (0, groq_1.default) `*[_type == "apiVersion" && platform._ref == $platformId && semver == $semver]{ _id }`;
+        const existingVersions = await client.fetch(existingVersionsQuery, {
+            platformId: platform._id,
+            semver: version,
+        });
+        const attachment = {
+            _type: 'file',
+            asset: {
+                _type: 'reference',
+                _ref: uploadedAsset._id,
+            },
+        };
+        let res;
+        if (existingVersions.length > 0) {
+            core.info(`[Patching] ${existingVersions.length} existing apiVersion document(s)`);
+            const transaction = client.transaction();
+            for (const doc of existingVersions) {
+                core.info(`  - ${doc._id}`);
+                transaction.patch(doc._id, { set: { attachment } });
+            }
+            res = await transaction.commit();
+        }
+        else {
+            core.info(`[Creating] New apiVersion document for ${packageName} v${version}`);
+            res = await client.create({
+                _type: 'apiVersion',
+                date: new Date().toISOString().split('T')[0],
+                platform: {
+                    _type: 'reference',
+                    _ref: platform._id,
+                },
+                semver: version,
+                attachment,
+            });
+        }
+        core.info(`[Uploaded] Typedoc JSON for ${packageName} v${version}`);
+        console.log(JSON.stringify(res, null, 2));
     }
-    core.info(`[Uploaded] Typedoc JSON for ${packageName} v${version}`);
-    console.log(JSON.stringify(res, null, 2));
+    catch (error) {
+        core.setFailed(`Error uploading typedoc JSON: ${error}`);
+    }
 }
 if (!process.env.VITEST) {
     run();
